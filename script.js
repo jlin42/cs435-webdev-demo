53b403d92354e0b5a2d8dd011150a0047a3c7702
jlin42 <jlin14@wm.edu>
2024-11-12 16:47:31 -0500

    Commit changes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
+
+function addTask(){
+    const taskInput = document.getElementById('taskInput').value;
+    const newTask = document.createElement('li');
+    newTask.innerText= taskInput;
+    const taskList = document.getElementById('taskList');
+    taskList.appendChild(newTask)
+    const deleteButton = document.createElement("button");
+    deleteButton.className="button";
+    deleteButton.innerText="x";
+    newTask.appendChild(deleteButton)
+    deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+    document.getElementById('taskInput').value=''
+}
 
